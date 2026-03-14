fd54227040a2ac19d452eb1e0bb46a5f44703e21
CarloDM <carlo.de.mauro.work@gmail.com>
2026-03-14 14:05:27 +0100

    tombola 2

--- classiMetodiEs/src/classiMetodiFileUno/Schedina.java
+++ classiMetodiEs/src/classiMetodiFileUno/Schedina.java
@@ -1,104 +1,89 @@
 package classiMetodiFileUno;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class Schedina {
 	public static void main(String[] args) {
 		// test
-		tornaSchedina(1, 90);
+		List<Integer> numEstrattiNow = new ArrayList<>();
+		int[][]  schedina =  new int[3][5];
+		int extractionCounter = 0;
+		
+
+		
+		for (int i = 0; i < 15; i++) {
+			System.out.println("for " + i );
+			
+			int numCasuale = EstrattoreNumCasuali.generaNumRandomDaA(1, 90);
+			
+			boolean isGiaEstratto = verificaSeGiaEstratto(numCasuale,numEstrattiNow);
+			
+			while (isGiaEstratto) {
+				numCasuale = EstrattoreNumCasuali.generaNumRandomDaA(1, 90);
+				isGiaEstratto = verificaSeGiaEstratto(numCasuale,numEstrattiNow);
+				System.out.println("riprova" + isGiaEstratto);
+			}
+			
+			numEstrattiNow.add(numCasuale);
+			System.out.println(numEstrattiNow);
+			
+		
+		// aggiungi a schedina magari usando un contatore per semplicita
+			if(extractionCounter >= 0 && extractionCounter <= 4) {
+				
+				System.out.println("prima riga");
+				schedina[0][extractionCounter] = numCasuale;
+				System.out.println(schedina[0][extractionCounter]);
+				extractionCounter++;
+				
+			} else if (extractionCounter >= 5 && extractionCounter <= 9) {
+				
+				System.out.println("seconda riga");
+				schedina[1][extractionCounter - 5] = numCasuale;
+				System.out.println(schedina[0][extractionCounter - 5]);
+				extractionCounter++;
+				
+			} else if (extractionCounter >= 10 && extractionCounter <= 14){
+				
+				System.out.println("terza riga");
+				schedina[2][extractionCounter - 10] = numCasuale;
+				System.out.println(schedina[0][extractionCounter - 10]);
+				extractionCounter++;
+				
+			}else {
+				System.err.println("numero estratto non valido");
+			}	
+		}//for
+		System.out.println("schedina lenght 1 " + schedina[0].length);
+		
+		for (int i = 0; i < schedina.length ; i++) {
+			
+			System.out.println("schedina riga " + i);
+			
+			for (int j = 0; j < schedina[i].length ; j++) {
+
+				System.out.print(schedina[i][j] + " ");
+			}
+			System.out.println(" ");
+		}
+		
+		
+		
 	} // MAIN
 	
-	
-	// usa estrattore per raccogliere numeri casuali e non ripetibili 
-	// in 3 righe da 5 colonne di numeri non ripetibili in tutte le righe... come schedina della topmbola.
-	// qui potrei implementare primo ciclo wild che chiede una serie di numeri non ripetuti
-	
-	public static void tornaSchedina(int min, int max) {
+	public static boolean verificaSeGiaEstratto (int nRnd, List<Integer> estratti) {
+		boolean res = estratti.contains(nRnd);
 		
-	//creare 1 array composto di 3 array composti di 5 cifre int diverse tra 1 e 90 presenti in tutti i sotto array di schedina cioe':
-	//univoco rispetto tutti i numeri presenti nell array schedina, non puo comparire 2 volte lo stesso numero in tutto array schedina.
-		
-		
-	int[][]  schedina =  new int[3][5];	
-	// ci vuole un array dinamico dei numeri gia estratti bisoga importare : java.util.ArrayList;
-	// <Integer> = tipo degli elementi contenuti nella lista. quest integer sembrerebbe esser un oggetto quindi chiamato tra < e >	
-	// ArrayList<> e' un array non dinamico.
-	
-	ArrayList<Integer> numEstrattiNow = new ArrayList<>();
-	
-	
-		
-//	-=----------------------------------------------------------------------------------------FUNZIONE asegna solo un numero 	
-	//dato un arrayDinamico anche vuoto produci un numero non esistente nell arrayDinamico e aggiongilo
-	//questa funzione deve ricevere un array  e restituire un array
-	//quindi devo generare un numero e scartarlo se presente in numeriEstrattNow, se no aggiungierlo.
-	int nRnd = EstrattoreNumCasuali.generaNumRandomDaA(1,90); // compresi
-	System.out.println(nRnd);
-		
-		
-	//------> funzione assegnaaione numero random agli estratti
-	// porta per rendere valido o gia estratto il numero ... flag.
-	boolean nRndValido = true;
-	
-	//se ancora nessun numero estratto...
-	if(numEstrattiNow.isEmpty()) {
-			
-		System.out.println("nessuna prima estrazione" );
-		
-		//fatta prima estrazione e inserire numero nell array
-		numEstrattiNow.add(nRnd);
-		System.out.println(numEstrattiNow);
-		
-	// DAnGER--------
-	}else{ // occhio serve sorting se no non funziona bene perche non ricerca; solo cicla le 90 possibilita' ...sequenzialmente da 1 a 90.
-		   //per funzionare correttamente ha bisogno di un array di int ordinati dal piu grande al piu piccolo ...dimodo che possa controllare tutte le possibilita'
-		System.out.println("else???? ciclo controllo");
-		//dando per scontato che abbiamo un array di numeri int ordinati dal piu grande al piu' piccolo.... 
-		//lopzione index 0 deve essere la maggiore possibile nell array e discendendo le altre opzioni...
-		for (int i = 0; i <= 89; i++) {
-			// sto usando arrayList quindi devo per accedere all indice usare metodo .get(index)
-			if(nRnd ==  numEstrattiNow.get(i)) {
-				// trovata corrispondenza
-				// non assegnare numero fermarsi...
-				nRndValido = false;
-				break;
-			}
-		}//for
-	}
-	
-	if(!nRndValido) {
-		
-		System.out.println("numero gia estratto riprovare....");
-		
-	}else{
-				// Solo se proprio non c e' aggiungilo. POST-CONTROLLO. 
-				if(numEstrattiNow.isEmpty()) { 
-					
-					System.out.println("nessuna prima estrazione SECONDO controllo" );
-					//inserire numero nell array
-					numEstrattiNow.add(nRnd);
-					System.out.println(numEstrattiNow);
-					System.out.println("secondo controlloooo!!!!!");
-				};
-				
-				System.out.println("aggiunto numero " + nRnd + " ad schedinaNow"  + numEstrattiNow );
+		return res;
 		
 	};
+	// usa estrattore per raccogliere numeri casuali e non ripetibili 
+	// in 3 righe da 5 colonne di numeri non ripetibili in tutte le righe... come schedina della topbola.
+	// qui potrei implementare primo ciclo wild che chiede una serie di numeri non ripetuti
 	
-//	-=----------------------------------------------------------------------------------------FUNZIONE	
-	
-	
-	
-	
-	// funzione assegnaaione numero random agli estratti <------
-		
-	// eseguire estrazione dei 5 numeri 3 volte senza mai ripetersi?
-	//mi serve un operatore di confronto ciclato alla lunghezza dell array numeriEstrattNow.
-		
-	//int[] numero1 = numEstrattiNow;
-	
-	}// tornaSchedina();
+
 	
 	
 }//class
