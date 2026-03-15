b0ce2d88b56ab311d173250cf4db6b31aaa4c7c2
CarloDM <carlo.de.mauro.work@gmail.com>
2026-03-15 15:19:51 +0100

    clean code

--- classiMetodiEs/src/classiMetodiFileUno/Tombola.java
+++ classiMetodiEs/src/classiMetodiFileUno/Tombola.java
@@ -3,63 +3,51 @@ package classiMetodiFileUno;
 import java.util.ArrayList;
 import java.util.List;
 
-// simula il gioco della tombola reale in modo automatico
+// simula il gioco della tombola con regole simili al reale in modo automatico
 // stampa in console i risultati e procedimenti importanti
-
-
 // abbiamo il giocatore con la sua unica schedina
 // giocatore sa verificare se numero dato e' presente nella sua schedina
 // sa flaggare i propri numeri presenti per ogni riga della sua schedina
-// sa verificare e dichiarare se ha fatto ambo tris quadris o tombola
+// sa verificare e dichiarare se ha fatto ambo terno quaterna o tombola
 
-// abbiamo il Banco con l array dei numeri estratti
+// abbiamo il Banco con l'array dei numeri estratti
 // sa escludere i numeri random gia estratti
 // verifica le condizioni di vittoria
-// avvia l estrazione e le verifiche
+// avvia l'estrazione e le verifiche
 // avvisa delle vincite
 // chiude la partita quando almeno un giocatore ha fatto tombola while
 
-
 public class Tombola {
 	
-	
-	
-	
+	// variabili dichiarate fuori main
 	static final int 	  numeroGiocatori    = DidacticData.STUDENTI_22418.length;
 	static List<Integer> numEstratti        = new ArrayList<>();
 	static int[][][]     schedineGiocatori  = new int[numeroGiocatori][3][5];
 	static boolean[][]   risultatiGiocatori = new boolean[numeroGiocatori][4];
 	static boolean       isPartitaFinita    = false;
 	
-	
-	
-	
-	
 	public static void main(String[] args) throws InterruptedException {
 
 		ciaoTombola();
 		isPartitaFinita = false;
+		// generare schedine per giocatore
 		schedineGiocatori = tornaSchedine(schedineGiocatori);
 		
 		while (!isPartitaFinita) {
 			turnoStep();
 			//intervallare temporalmente il ciclo
-			Thread.sleep(600);
+			Thread.sleep(320);
 		}
 		
 	}// main
 	
 	public static void turnoStep() {
 		
-		// generare schedine per giocatore
-		
-		
 		// estrai un numero con verifica di non ripetizione
 		numEstratti.add(tornaNumNonRipetuto(numEstratti));
 		
 		// ogni giocatore deve verificare se ha nella schedina i numeri estratti
-
-		if(numEstratti.size() >= 2) {
+		if(numEstratti.size() >= 2) {//salto prima estrazione
 			
 			risultatiGiocatori = giocatoriNumEstratti(numeroGiocatori, numEstratti, schedineGiocatori, risultatiGiocatori );
 			
@@ -70,23 +58,24 @@ public class Tombola {
 				for (int j = 3; j >= 0; j--) {
 					
 					if(risultatiGiocatori[i][3]){
-//						System.out.print(DidacticData.STUDENTI_22418[i ]);
+
 						System.out.print("_TOMBOLA!!!");
 						System.out.print(" ==== PARTITA FINITA ==== ");
 						isPartitaFinita = true;
+						
 						break;
 					}else if(risultatiGiocatori[i][2]) {
-//						System.out.print(DidacticData.STUDENTI_22418[i ]);
+
 						System.out.print("QUATERNA!!");
 						
 						break;
 					}else if(risultatiGiocatori[i][1]) {
-//						System.out.print(DidacticData.STUDENTI_22418[i ]);
+
 						System.out.print("TERNA!!");
 						
 						break;
 					}else if(risultatiGiocatori[i][0]) {
-//						System.out.print(DidacticData.STUDENTI_22418[i]);
+
 						System.out.print("AMBO!!");
 						
 						break;
@@ -95,15 +84,21 @@ public class Tombola {
 				}// for in for	
 				System.out.println(" ");
 			}// for
-			
 		}// if numeri estratti maggiore di 1
+		
+		
+		//step fase  finale stampe
 		System.out.println(" ");
-		System.err.println("NUM EXT : " + numEstratti.getLast().toString());
+		
+		// prima di ordinare osservo l ultimo numero nell array estratti
+		System.err.println("ULTIMO ESTRATTO : " + numEstratti.getLast().toString());
+		
 		// si puo velocemente mettere in ordine per maggior leggibilita
 		numEstratti.sort(null);
 		
+		// PRINTI NUMERI ESTRATTI
 		System.out.println(" ");
-		System.out.print("NUM EXTS  ");
+		System.out.print("NUM ESTRATTI: ");
 		System.out.println(numEstratti.toString());
 		System.out.println(" ");
 		
@@ -111,7 +106,8 @@ public class Tombola {
 	
 	
 	
-	public static boolean[][] giocatoriNumEstratti(int nGiocatori, List<Integer> nEstratti, int[][][] schedGiocatori, boolean[][] resGiocatori ){
+	public static boolean[][] giocatoriNumEstratti(int nGiocatori, 
+			List<Integer> nEstratti, int[][][] schedGiocatori, boolean[][] resGiocatori ){
 		
 		boolean[][] rGiocatori= resGiocatori;
 		
@@ -176,7 +172,7 @@ public class Tombola {
 	
 	public static void ciaoTombola(){
 	
-		System.out.println("ciao io sono tombola");
+		System.out.println("ciao io sono simulazione di tombola");
 		
 	};	
 	
